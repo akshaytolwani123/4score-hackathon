1026650ecd0691647ec5740f99be7eb0607afd99
Akshay Tolwani <akshaytolwanipython@gmail.com>
2026-07-16 14:12:12 +0530

    Add 4score.aksahytolwani.xyz to allowed domains for outside access

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], server: { proxy: { '/api': { target: 'http://api:8000', rewrite: p => p.replace(/^\/api/, '') } } } })
+export default defineConfig({ plugins: [react()], server: { allowedHosts: ["4score.akshaytolwani.xyz"], proxy: { '/api': { target: 'http://api:8000', rewrite: p => p.replace(/^\/api/, '') } } } })
